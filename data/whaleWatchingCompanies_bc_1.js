90480137f6275b6ee292f6b82c1ae48c3e4a166f
nesdolya <andreanesdoly@gmail.com>
2019-05-31 11:23:39 -0700

    First Commit

---
+++ data/whaleWatchingCompanies_bc_1.js
@@ -0,0 +1,15 @@
+var json_whaleWatchingCompanies_bc_1 = {
+"type": "FeatureCollection",
+"name": "whaleWatchingCompanies_bc_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Company": "Eagle Wing Tours", "Website": "https:\/\/www.eaglewingtours.com\/" }, "geometry": { "type": "Point", "coordinates": [ -123.38242373816918, 48.422548460117397 ] } },
+{ "type": "Feature", "properties": { "Company": "Ocean Ecoventures", "Website": "https:\/\/oceanecoventures.com\/" }, "geometry": { "type": "Point", "coordinates": [ -123.619121619154612, 48.740402472610441 ] } },
+{ "type": "Feature", "properties": { "Company": "Jamie's Whale Station Tofino", "Website": "https:\/\/www.jamies.com\/" }, "geometry": { "type": "Point", "coordinates": [ -125.898340169960193, 49.150979720553721 ] } },
+{ "type": "Feature", "properties": { "Company": "Jamie's Whale Station Ucluelet", "Website": "https:\/\/www.jamies.com\/" }, "geometry": { "type": "Point", "coordinates": [ -125.545035277040853, 48.94286168680555 ] } },
+{ "type": "Feature", "properties": { "Company": "Remote Passages", "Website": "http:\/\/www.remotepassages.com\/" }, "geometry": { "type": "Point", "coordinates": [ -125.907224167686877, 49.154662721466686 ] } },
+{ "type": "Feature", "properties": { "Company": "Ocean Outfitters", "Website": "https:\/\/www.oceanoutfitters.bc.ca\/" }, "geometry": { "type": "Point", "coordinates": [ -125.905237168174622, 49.153914721259753 ] } },
+{ "type": "Feature", "properties": { "Company": "The Whale Centre", "Website": "https:\/\/www.tofinowhalecentre.com\/" }, "geometry": { "type": "Point", "coordinates": [ -125.904327168720968, 49.152389721207051 ] } },
+{ "type": "Feature", "properties": { "Company": "Subtidal Adventures", "Website": "http:\/\/www.subtidaladventures.com\/" }, "geometry": { "type": "Point", "coordinates": [ -125.556214275022398, 48.944367688052161 ] } }
+]
+}
